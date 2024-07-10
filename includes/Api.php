726580ce36848c1307bacbace51a404a61379d3c
EmilianoCode <emilianomm223@gmail.com>
2024-07-10 17:02:24 -0300

    changes

---
+++ includes/Api.php
@@ -0,0 +1,37 @@
+<?php
+
+    namespace ClashRoyaleAPI\includes;
+
+    class Api {
+
+        private $token;
+        private $headers;
+        private $url;
+        private $category;
+        private $tag;
+    
+        public function __construct($category, $tag) {
+            $this->category = $category;
+            $this->tag = $tag;
+            $this->url = "https://api.clashroyale.com/v1/".$this->category."/".$this->tag;
+            $this->token = getenv("CRA-TOKEN");
+            $this->headers = ["Accept: application/json", "Authorization: Bearer ".$this->token];
+        }
+
+
+        public function apiResult(){
+            $curl_handle =  curl_init($this->url);
+            curl_setopt($curl_handle, CURLOPT_HTTPHEADER, $this->headers);
+            curl_setopt($curl_handle, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt($curl_handle, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($curl_handle, CURLOPT_RETURNTRANSFER, 1);
+
+            $result = curl_exec($$curl_handle);
+            $data = json_decode($result,1);
+
+            curl_close($curl_handle);
+
+            return $data;
+        }
+    }
+?>
--- index.php
+++ index.php
@@ -10,42 +10,22 @@
     </title>
 </head>
 <body>
-    <?php //$miVariable = getenv('MI_LOQUITA');
-    header('Content-Type: text/html; charset=UTF-8');
-    $token = getenv("CRA-TOKEN");
-    
-    $url = "https://api.clashroyale.com/v1/players/%238RYPQ9RQG";
-    
-    $ch = curl_init($url);
-    
-    $headr = array();
-    $headr[] = "Accept: application/json";
-    $headr[] = "Authorization: Bearer ".$token;
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headr);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-    $res = curl_exec($ch);
-    //$tojson = json_encode($res);
-    $data = json_decode($res,1);
-    curl_close($ch);
-    
-    echo "<h3>".$data["reason"]."</h3>";
-    /*function test_input($form_var) {
-    $data = trim($form_var);
-    $data = stripslashes($form_var);
-    $data = htmlspecialchars($form_var);
-    return $form_var;
-    }
-    if (isset($data["reason"])) {
-    echo "<p>", "Failed: ", $data["reason"], " : ", isset($data["message"]) ? $data["message"] : "", "</p></body></html>";
-    exit;
-    }*/
-    
-    //echo "<p>";
-    //echo var_dump($data);
-    //echo $res;
-    //echo $data["name"];
+    <?php 
+    require "includes/Api.php";
+
+    header('Content-Type: text/html; charset=UTF-8'); 
+
+    use ClashRoyaleAPI\includes\Api;
+   
+    $api = new Api("players","%238RYPQ9RQG");
+
+    $data = $api->apiResult();
+
+    // JSON LOCAL
+    /*$pathfile = 'json-models/userdata.json';
+    $jsonString = file_get_contents($pathfile);
+    $data = json_decode($jsonString, true);*/
+
     echo '<div style="width:90%; background:lightblue; display:flex; flex-direction:column; margin:auto; padding:30px; box-sizing:border-box; border-radius: 20px;">';
     echo '<h1>VAMOS A PONERNOS SERIOS</h1>';
     echo '<h3 style="font-family:Open Sans; font-size: 23px;"> Jugador: '.$data["name"].'</h3>';
