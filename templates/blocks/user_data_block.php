31af1a0b90557d9d7f710f99bb6e6d1054170104
EmilianoCode <emilianomm223@gmail.com>
2024-07-13 13:15:44 -0300

    changes

--- templates/blocks/user_data_block.php
+++ templates/blocks/user_data_block.php
@@ -4,7 +4,7 @@ function general_player_data_block($userdata){
 
     $block = '<ul class="list-group">';
 
-    for($userdata as $key => $value){
+    foreach($userdata as $key => $value){
         $block .= '<li class="list-group-item d-flex justify-content-between align-items-center">'
                     .$key.
                     '<span class="badge text-bg-primary rounded-pill">'
